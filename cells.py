ff1a57e8793eba623416684c54713754b7c4b962
Athena Economides <athena.economides@uzh.ch>
2023-05-02 16:00:14 +0200

    cell boundary segmentation

--- cells.py
+++ cells.py
@@ -13,26 +13,18 @@ args = parser.parse_args()
 
 
 for image_file in args.i:
-    # load input nuclei image
+    # load input image
     img = skimage.io.imread(image_file, plugin='tifffile')
-    print(np.median(img), np.min(img), np.max(img))
-
-    # cap values
-    threshold = np.percentile(img, 98)
-    capped = np.zeros(np.shape(img))
-    capped[:,:] = img[:,:]
-    capped[img>threshold] = threshold
 
     # background subtraction
-    back = scipy.ndimage.gaussian_filter(capped, sigma=100, mode='reflect')
-    print(np.median(back), np.min(back), np.max(back))
+    back = scipy.ndimage.gaussian_filter(img, sigma=100, mode='reflect')
 
     # normalized
     norm = img / back
     assert(np.min(norm)>=0)
 
-    # segment
-    threshold = np.percentile(norm, 98)
+    # segmented (binary mask)
+    threshold = skimage.filters.threshold_li(norm) 
     segmented_ = np.zeros(np.shape(img), dtype=np.uint8)
     segmented_[norm>threshold] = 1
 
@@ -44,12 +36,12 @@ for image_file in args.i:
     print("Connected cmoponents (original):", np.max(labels))
 
     # remove small holes
-    noholes = skimage.morphology.remove_small_holes(segmented, area_threshold=400, connectivity=2)
+    noholes = skimage.morphology.remove_small_holes(segmented, area_threshold=5*5, connectivity=2)
     labels = skimage.morphology.label(noholes, connectivity=2)
     print("Removed small holes:", np.max(labels))
 
     # remove small objects
-    nosmall = skimage.morphology.remove_small_objects(labels, min_size=9, connectivity=2)
+    nosmall = skimage.morphology.remove_small_objects(labels, min_size=20*20, connectivity=2)
     labels = skimage.morphology.label(nosmall, connectivity=2)
     print("Removed small objects:", np.max(labels))
     obj = np.max(labels)
@@ -61,5 +53,5 @@ for image_file in args.i:
         os.makedirs(opath)
     labels = np.asarray(labels, dtype=np.uint32)
     assert(np.max(labels) == obj)
-    skimage.io.imsave("%s/%s_labels_aggregates.tif" % (opath, bpath), labels, plugin='tifffile')
+    skimage.io.imsave("%s/%s_labels_cells.tif" % (opath, bpath), labels, plugin='tifffile')
 

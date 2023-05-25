08d7b0fc7d9476096c30185c48a0b35564062453
Athena Economides <athena.economides@uzh.ch>
2023-05-25 15:55:07 +0200

    version 0.0: find all tif files inside pre-specified directory

---
+++ version_00/README.md
@@ -0,0 +1,19 @@
+# Version 0.0
+
+First attempt of a multi-plate analysis, for the segmentation of nuclei, cell bodies and a-Syn aggregates.
+
+
+## Instructions
+
+Specify the path to the directory containing the tif images, and
+the identifiers for the Nuclei, Aggregates and Cells, inside `paths.txt`.
+
+An example for how `paths.txt` should look like is:
+
+```
+PATH_TO_IMAGES="/Documents/athena/trial_20230525_01"
+COLOUR_NUCLEI="Blue"
+COLOUR_AGGREGATES="Green2"
+COLOUR_CELLS="FarRed"
+```
+
---
+++ version_00/main.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import glob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', type=str, required=True, help="text file with path to tifs, and colour identifiers")
+args = parser.parse_args()
+
+
+if os.path.exists(args.i):
+    print("\nReading paths and colours from file: %s \n" % args.i)
+else:
+    print("File %s does NOT exist! Give a valid path to a file." % args.i)
+    sys.exit()
+
+
+with open(args.i) as file:
+    for line in file:
+
+        if line.find("PATH_TO_IMAGES")==0:
+            path_to_dir = (line.split(sep="=")[1].rstrip()).replace("\"", "")
+
+            if os.path.exists(path_to_dir) == True:
+                print("Found directory: %s" % path_to_dir)
+            else:
+                print("Directory %s does NOT exist! Make sure that there are no spaces before/after the = symbol in %s" % (path_to_dir, args.i))
+                sys.exit()
+
+        if line.find("COLOUR_NUCLEI")==0:
+            CNUCLEI = (line.split(sep="=")[1].rstrip()).replace("\"", "")
+            print("COLOUR_NUCLEI = %s" % CNUCLEI)
+
+        if line.find("COLOUR_AGGREGATES")==0:
+            CAGGREGATES = (line.split(sep="=")[1].rstrip()).replace("\"", "")
+            print("COLOUR_AGGREGATES = %s" % CAGGREGATES)
+
+        if line.find("COLOUR_CELLS")==0:
+            CCELLS = (line.split(sep="=")[1].rstrip()).replace("\"", "")
+            print("COLOUR_CELLS = %s" % CCELLS)
+
+
+print("\nRunning nuclei segmentation.")
+images_nuclei = glob.glob("%s/*%s*.tif" % (path_to_dir, CNUCLEI))
+print("Found %d nuclei images." % len(images_nuclei))
+os.system("conda run -n tf python nuclei.py -i %s" % (images_nuclei))
+
+

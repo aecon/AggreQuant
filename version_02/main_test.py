4fe811844e4f084b491329b32b1f50e2ed77a54e
Athena Economides <athena.economides@uzh.ch>
2023-06-26 11:52:09 +0200

    minor

--- version_02/main_test.py
+++ version_02/main_test.py
@@ -5,6 +5,7 @@ import argparse
 from utils.parser import FileParser
 from processing.segmentation import ImageProcessor
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, required=True, help="paths.txt file with path to tifs, and colour identifiers")
 parser.add_argument('-debug', action='store_true')
@@ -12,10 +13,10 @@ parser.add_argument('-verbose', action='store_true')
 args = parser.parse_args()
 
 
+# File parser
 fileParser = FileParser(args)
 
-
-# Image Processing
+# Image processing
 processor = ImageProcessor(fileParser)
 
 

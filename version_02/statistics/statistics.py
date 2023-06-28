c671da48bc2d9a5f4a170cece38ddb20d8d6a51c
Athena Economides <athena.economides@uzh.ch>
2023-06-28 18:06:22 +0200

    draft:plot percentage of aggregate positive cells

--- version_02/statistics/statistics.py
+++ version_02/statistics/statistics.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import numpy as np
-import re
+import matplotlib.pyplot as plt
 
 from processing.dataset import Dataset
 from statistics.plate import Plate
@@ -25,10 +25,6 @@ class Statistics:
     #        self.table[i,:] = data[:]
 
 
-    def _percent_aggregate_positive_cells(self):
-        print("TODO")
-
-
     def _load_QoI_whole_plate(self):
         print("TODO.")
 
@@ -43,24 +39,20 @@ class Statistics:
 
                 # patter matching to find correct aggregate filename..
                 # - match row
-                pattern = ".*_%s *" % row_letter
-                expression = re.compile(pattern)
-                sublistR = list(filter(expression.match, list_of_files))
+                pattern = "_%s " % row_letter
+                sublistR = [x for x in list_of_files if pattern in x]
                 # - match column
-                pattern = ".*- %s*" % ControlColumn
-                expression = re.compile(pattern)
-                files_all_fields_per_well = list(filter(expression.match, sublistR))
+                pattern = "- %s" % ControlColumn
+                files_all_fields_per_well = [x for x in sublistR if pattern in x]
                 assert(len(files_all_fields_per_well)<=self.plate.Nfields)
 
                 # initialize Well
-                column = int(ControlColumn)
+                column = int(ControlColumn) - 1 # numbering starts with 0!
                 global_index = self.plate.get_global_well_number(row, column)
                 self.plate.wells[global_index] = [None] * self.plate.Nfields
 
-                # fill in QoI for all fields in well
+                # fill in QoI for all fields in Well
                 for field, file_a in enumerate(files_all_fields_per_well):
-                    print(file_a)
-
                     # load QoI results
                     results_dict = self.dataset.get_output_file_names(file_a, "QoI")
                     data = np.loadtxt(results_dict["QoI"], skiprows=1)
@@ -69,15 +61,50 @@ class Statistics:
                     self.plate.wells[global_index][field] = data
 
 
+    def _percent_aggregate_positive_cells_Controls(self):
+
+        group_5Up  = np.zeros(8)
+        group_5Dn  = np.zeros(8)
+        group_13Up = np.zeros(8)
+        group_13Dn = np.zeros(8)
+
+        for i in range(8):
+            global_index = self.plate.get_global_well_number(i, 4)
+            data = np.asarray(self.plate.wells[global_index])
+            QoI = data[:,0]
+            group_5Up[i] = np.mean(QoI)
+
+            global_index = self.plate.get_global_well_number(i+8, 4)
+            data = np.asarray(self.plate.wells[global_index])
+            QoI = data[:,0]
+            group_5Dn[i] = np.mean(QoI)
+
+            global_index = self.plate.get_global_well_number(i, 12)
+            data = np.asarray(self.plate.wells[global_index])
+            QoI = data[:,0]
+            group_13Up[i] = np.mean(QoI)
+
+            global_index = self.plate.get_global_well_number(i+8, 12)
+            data = np.asarray(self.plate.wells[global_index])
+            QoI = data[:,0]
+            group_13Dn[i] = np.mean(QoI)
+
+
+        plt.scatter(1*np.ones(8), group_5Up, label="NT_1")
+        plt.scatter(2*np.ones(8), group_5Dn, label="Rab13_1")
+        plt.scatter(3*np.ones(8), group_13Dn, label="NT_2")
+        plt.scatter(4*np.ones(8), group_13Up, label="Rab13_2")
+        plt.ylim([0,80])
+        plt.legend()
+#        plt.show()
+        plt.close()
+
+
     def generate_statistics(self):
 
         # Load QoI files for the plate
         self._load_QoI_Controls()
 
-        assert(0)
-
-
         # QoI 1: Percentage of Aggregate Positive Cells
-        self._percent_aggregate_positive_cells()
-
+        self._percent_aggregate_positive_cells_Controls()
 

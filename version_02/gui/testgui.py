ba1032a4047dd35dfbe9b72251e239e5ddd77d18
Athena Economides <athena.economides@uzh.ch>
2023-10-19 16:40:55 +0200

    working dialog box

--- version_02/gui/testgui.py
+++ version_02/gui/testgui.py
@@ -2,80 +2,100 @@ import tkinter as tk
 from tkinter import ttk, filedialog
 from tkinter import *
 
-window = tk.Tk()
 
-window.title('aSynuclein processing')
-window.geometry('600x200')
+class GUI:
 
-window.columnconfigure([0, 1, 2], minsize=100)
-window.rowconfigure([0, 1, 2, 3], minsize=30)
+    def __init__(self):
+        self.window = tk.Tk()
+        self.input_folder = tk.StringVar(self.window, value="")
+        self.entry_width = 30
+        self.colour_nuc = tk.StringVar(self.window, value="")
+        self.colour_cel = tk.StringVar(self.window, value="")
+        self.colour_agg = tk.StringVar(self.window, value="")
 
-
-# Directory selection
-folder_path = StringVar(window, value="")
-
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    filename = filedialog.askdirectory(parent = window,
-        initialdir="/media/neptun/LocalDisk16TB", title="Dialog box")
-    folder_path.set(filename)
-
-label0 = tk.Label(text="Input directory containing all tif files:")
-label0.grid(row=0, column=0)
-#entry0 = tk.Entry(width=30, textvariable=folder_path)
-#entry0.grid(row=0, column=1)
-label0 = tk.Label(textvariable=folder_path)
-label0.grid(row=0, column=1)
-button1 = ttk.Button(window, text="Browse", command=browse_button)
-button1.grid(row=0, column=2)
+        self._initialize()
+        self._makelayout()
 
 
-#Create a label and a Button to Open the dialog
-#dialog_btn = ttk.Button(window, text="select directory", command=directory)
-#dialog_btn.grid(row=0, column=0)
+    def _initialize(self):
+        self.window.title('aSynuclein processing')
+        self.window.geometry('600x200')
+        self.window.columnconfigure([0, 1, 2], minsize=100)
+        self.window.rowconfigure([0, 1, 2, 3], minsize=30)
 
 
-entry_width = 30
-
-# Nuclei filename
-my_row = 1
-label2 = tk.Label(text="Filename label for nuclei")
-label2.grid(row=my_row, column=0, sticky="nw")
-entry1 = tk.Entry(width=entry_width)
-entry1.grid(row=my_row, column=1, sticky="nw")
-colour_nuclei = entry1.get()
-
-# Cells filename
-my_row = 2
-label3 = tk.Label(text="Filename label for nuclei")
-label3.grid(row=my_row, column=0, sticky="nw")
-entry3 = tk.Entry(width=entry_width)
-entry3.grid(row=my_row, column=1, sticky="nw")
-colour_cells = entry3.get()
-
-# Aggregates filename
-my_row = 3
-label4 = tk.Label(text="Filename label for aggregates")
-label4.grid(row=my_row, column=0, sticky="nw")
-entry4 = tk.Entry(width=entry_width)
-entry4.grid(row=my_row, column=1, sticky="nw")
-colour_aggregates = entry4.get()
+    def _browse_button(self):
+        # Allow user to select a directory and store it in global var
+        # called folder_path
+        filename = filedialog.askdirectory(parent = self.window,
+            initialdir="/media/neptun/LocalDisk16TB", title="Dialog box")
+        self.input_folder.set(filename)
 
 
-# Quits the program when certain button is clicked
-button2 = tk.Button(window, text="OK", command=window.quit)
-button2.grid()
+    def _close(self):
+        print("running _quit")
+        self.colour_nuc.set(self.entry_nuc.get())
+        self.colour_cel.set(self.entry_cel.get())
+        self.colour_agg.set(self.entry_agg.get())
+        self.window.quit()
 
-print("\nSummary of user choices:'")
-print("Nuclei label:", colour_nuclei)
-print("Cell label:", colour_cells)
-print("Aggregates label:", colour_aggregates)
-print("Selected input folder:", folder_path.get())
 
-# window.mainloop() tells Python to run the
-# Tkinter event loop. This method listens for 
-# events, such as button clicks or keypresses
-window.mainloop()
+    def _makelayout(self):
 
+        row = 0
+
+        # Directory selection
+        label0 = tk.Label(text="Input directory containing all tif files:")
+        label0.grid(row=row, column=0)
+
+        label1 = tk.Label(textvariable=self.input_folder)
+        label1.grid(row=row, column=1)
+
+        button1 = ttk.Button(self.window, text="Browse", command=self._browse_button)
+        row += 1
+        button1.grid(row=1, column=0, sticky="ne")
+
+        # Nuclei filename
+        row += 1
+        label_nuc = tk.Label(text="Filename label for nuclei")
+        label_nuc.grid(row=row, column=0, sticky="nw")
+
+        self.entry_nuc = tk.Entry(width=self.entry_width)
+        self.entry_nuc.grid(row=row, column=1, sticky="nw")
+
+        # Cells filename
+        row += 1
+        label_cel = tk.Label(text="Filename label for nuclei")
+        label_cel.grid(row=row, column=0, sticky="nw")
+
+        self.entry_cel = tk.Entry(width=self.entry_width)
+        self.entry_cel.grid(row=row, column=1, sticky="nw")
+
+        # Aggregates filename
+        row += 1
+        label_agg = tk.Label(text="Filename label for aggregates")
+        label_agg.grid(row=row, column=0, sticky="nw")
+
+        self.entry_agg = tk.Entry(width=self.entry_width)
+        self.entry_agg.grid(row=row, column=1, sticky="nw")
+
+        # Close the dialog when button is clicked
+        button_OK = tk.Button(self.window, text="OK", command=self._close)
+        button_OK.grid()
+
+        # run the tkinter event loop. It listens for 
+        # events, such as button clicks or keypresses
+        self.window.mainloop()
+        self.window.destroy()
+
+
+
+
+gui = GUI()
+
+
+print("input folder:", gui.input_folder.get())
+print("label nuclei:", gui.colour_nuc.get())
+print("label cells:", gui.colour_cel.get())
+print("label aggregates:", gui.colour_agg.get())
 

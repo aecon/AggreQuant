16344cbbd7d3d0912e31d19fd80aa452e9df0b76
Athena Economides <athena.economides@uzh.ch>
2023-11-02 21:18:15 +0100

    upd/add paths files

---
+++ version_02/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate5.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate6.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate7_HA4rep1.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate8_HA4rep2.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA7_rep1.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA7_rep2.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA8_rep1.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA8_rep2.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA13_rep1.txt
+#python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA13_rep2.txt
+
+

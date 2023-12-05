f091083d1f5c35339d42a390ac0cff7f7313053c
Athena Economides <athena.economides@uzh.ch>
2023-12-05 16:45:41 +0100

    upd paths.txt in run.sh

--- run.sh
+++ run.sh
@@ -8,19 +8,19 @@
 
 
 ## HA_3: Relatively good plates
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate5.txt
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate6.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_Plate5.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_Plate6.txt
 ## HA_4: Terrible plates: rep1 has very few cells, rep2 has too many
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate7_HA4rep1.txt
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_Plate8_HA4rep2.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_Plate7_HA4rep1.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_Plate8_HA4rep2.txt
 ## HA_7: Good plate
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA7_rep1.txt
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA7_rep2.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA7_rep1.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA7_rep2.txt
 ## HA_8: Good plate
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA8_rep1.txt
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA8_rep2.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA8_rep1.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA8_rep2.txt
 ## HA_13: Good plate
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA13_rep1.txt
-python3 main_test.py -validation -overwrite_output_folder -i paths_test_Controls_HA13_rep2.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA13_rep1.txt
+python3 main_test.py -validation -overwrite_output_folder -i paths/paths_test_Controls_HA13_rep2.txt
 
 

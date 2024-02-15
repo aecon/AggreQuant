f081e6154a77929dff193dc1826d5045f70cca72
Athena Economides <athena.economides@uzh.ch>
2024-02-15 15:37:53 +0100

    generate montage for control columns

--- processing/montage.py
+++ processing/montage.py
@@ -7,6 +7,27 @@ from utils.dataset import Dataset
 from statistics.diagnostics import *
 
 
+def _get_control_filepaths(all_raw, all_seg):
+
+    paths_Col05 = sorted([s for s in all_seg if "- 05(fld 5" in s])
+    paths_Col13 = sorted([s for s in all_seg if "- 13(fld 5" in s])
+    paths_NT1 = paths_Col05[0:8]
+    paths_NT2 = paths_Col13[8::]
+    paths_RB1 = paths_Col05[8::]
+    paths_RB2 = paths_Col13[0:8]
+    paths_seg_nuclei = paths_NT1 + paths_NT2 + paths_RB1 + paths_RB2
+
+    paths_Col05 = sorted([s for s in all_raw if "- 05(fld 5" in s])
+    paths_Col13 = sorted([s for s in all_raw if "- 13(fld 5" in s])
+    paths_NT1 = paths_Col05[0:8]
+    paths_NT2 = paths_Col13[8::]
+    paths_RB1 = paths_Col05[8::]
+    paths_RB2 = paths_Col13[0:8]
+    paths_raw_nuclei = paths_NT1 + paths_NT2 + paths_RB1 + paths_RB2
+
+    return paths_raw_nuclei, paths_seg_nuclei
+
+
 def montage(dataset, verbose, debug):
     """
     Makes a montage (i.e. a grid of images) with the aim of
@@ -76,6 +97,9 @@ def montage(dataset, verbose, debug):
                 dataset.paths_nuclei, montage_filename, 
                 debug=debug)
 
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Montage for whole plate
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # overlay nuclei
         print("Generating montage for nuclei segmentation\n")
         montage_filename = "%s/montage_overlay_nuclei.tif" % (
@@ -105,4 +129,26 @@ def montage(dataset, verbose, debug):
         montage_overlay_two_images(
             dataset.paths_aggregates, paths_seg_agg, montage_filename, rand, genRand=False,
             debug=False, verbose=False)
+
+
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Montage for Control columns
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        print("Generating CONTROLS montage for nuclei segmentation\n")
+        montage_filename = "%s/montage_controls_nuclei.tif" % (dataset.output_folder_diagnostics)
+        paths_raw, paths_seg = _get_control_filepaths( 
+            dataset.paths_nuclei, glob.glob("%s/*Blue*seeds*.tif" % (dataset.output_folder_nuclei)) )
+        montage_overlay_control_columns(paths_raw, paths_seg, montage_filename, verbose=False)
+
+        print("Generating CONTROLS montage for cells segmentation\n")
+        montage_filename = "%s/montage_controls_cells.tif" % (dataset.output_folder_diagnostics)
+        paths_raw, paths_seg = _get_control_filepaths( 
+            dataset.paths_cells, glob.glob("%s/*Red*labels*.tif" % (dataset.output_folder_cells)) )
+        montage_overlay_control_columns(paths_raw, paths_seg, montage_filename, verbose=False)
+
+        print("Generating CONTROLS montage for aggregate segmentation\n")
+        montage_filename = "%s/montage_controls_aggregates.tif" % (dataset.output_folder_diagnostics)
+        paths_raw, paths_seg = _get_control_filepaths( 
+            dataset.paths_aggregates, glob.glob("%s/*Green*labels*.tif" % (dataset.output_folder_aggregates)) )
+        montage_overlay_control_columns(paths_raw, paths_seg, montage_filename, verbose=False)
 
--- statistics/diagnostics.py
+++ statistics/diagnostics.py
@@ -55,6 +55,74 @@ def montage_simple(image_list, output_filename, debug=False, verbose=False):
     skimage.io.imsave("%s_montage%dx%d.tif" % (output_filename, panel_size[0], panel_size[1]), montage, plugin='tifffile', check_contrast=False)
 
 
+def montage_overlay_control_columns(images_raw, images_seg, output_filename, verbose=False):
+
+    Nfiles = len(images_raw)
+    assert(Nfiles == 32)
+
+    panel_size = [4, 8]   # row1:NT-1, row2:NT-2, row3:Rab13-1, row4:Rab13-2
+    Npixels = 1200
+    Nspace = 10
+
+    print("  Nraw", len(images_raw))
+    print("  Nseg", len(images_seg))
+    assert(len(images_seg) == len(images_raw))
+    assert(len(images_seg) == Nfiles)
+
+    # crop images
+    image_deck_seg = []
+    image_deck_raw = []
+    for r in range(Nfiles):
+        img0_seg = skimage.io.imread(images_seg[r], plugin='tifffile')
+        #img0_seg[img0_seg>0] = 1
+        # reconstruct edges from nuclei seeds
+        #mask = np.zeros(np.shape(img0_seg))
+        #mask[img0_seg>0] = 1
+        #edges0 = skimage.filters.sobel(mask)
+        #img0_seg = np.zeros(np.shape(edges0), dtype=np.dtype(np.uint8))
+        #img0_seg[edges0>0] = 1
+
+        img0_raw = skimage.io.imread(images_raw[r], plugin='tifffile')
+
+        #print(images_seg[r], images_raw[r])
+
+        assert(np.sum(np.shape(img0_seg)) == np.sum(np.shape(img0_raw)))
+        shape = np.shape(img0_seg)
+        L = shape[0] - Npixels
+        P0 = int(L/2)
+        img_seg = np.zeros((Npixels,Npixels))
+        img_seg[:,:] = img0_seg[P0:P0+Npixels, P0:P0+Npixels]
+        img_raw = np.zeros((Npixels,Npixels))
+        img_raw[:,:] = img0_raw[P0:P0+Npixels, P0:P0+Npixels]
+
+#        IMIN = np.min(img_raw)
+#        IMAX = np.max(img_raw)
+#        img_raw = (img_raw-IMIN) / (IMAX-IMIN)
+
+        image_deck_seg.append(img_seg)
+        image_deck_raw.append(img_raw)
+
+    # make montage
+    montage = np.zeros((2, panel_size[0]*Npixels+Nspace*(panel_size[0]-1) , panel_size[1]*Npixels+Nspace*(panel_size[1]-1) ) , dtype=np.dtype(np.uint16))
+    for i in range(panel_size[0]):
+        for j in range(panel_size[1]):
+            i0 = i*(Npixels+Nspace)
+            i1 = i0 + Npixels
+            j0 = j*(Npixels+Nspace)
+            j1 = j0 + Npixels
+            k = i*(panel_size[1]) + j
+            #tile = image_deck_raw[k][:,:]
+            #tile_s = image_deck_seg[k][:,:]
+            #tile[tile_s==1] = 65000
+            montage[0, i0:i1, j0:j1] = image_deck_raw[k][:,:]
+            montage[1, i0:i1, j0:j1] = image_deck_seg[k][:,:]
+
+    if verbose:
+        print("  Montage tif shape:", np.shape(montage))
+
+    # save montage
+    skimage.io.imsave("%s_%dx%d.tif" % (output_filename, panel_size[0], panel_size[1]), montage, plugin='tifffile', imagej=True, check_contrast=False)
+
 
 
 def montage_overlay_two_images(images_raw, images_seg, output_filename, rand=None, genRand=True, debug=False, verbose=False):

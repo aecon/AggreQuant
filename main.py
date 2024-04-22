ed0f8af1e8c6670d75a2088eea728b79d72ec23b
Athena Economides <athena.economides@uzh.ch>
2024-04-22 16:16:22 +0200

    clean main files

--- main.py
+++ main.py
@@ -6,34 +6,6 @@ from processing.pipeline import process
 from processing.montage import montage
 from processing.statistics import statistics
 
-
-"""
-HIGH CONTENT SCREEN IMAGE PROCESSING AND QUANTIFICATION
-
-Application:
-    main.py coordinates all operations:
-    - stores information about the dataset (input/output paths).
-    - stores information about currently processed data.
-    - gathers statistics for aggregate localization in cells.
-    - generates montage images for screen/segmentation assessment.
-
-Variables:
-    - verbose : display status messages in the standard output.
-    - debug : display additional messages for debugging.
-
-    - output_directory : choice for the name of the output directory.
-      The output directory is generated as a sub-directory inside the
-      input directory specified in the yml setup file.
-      Possible options:
-      * "date" : generates a new directory with current date/time.
-      * "debug" : generates/overwrites "output_debug" directory.
-      *  anything else inside quotes: a user defined name
-"""
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# SETTINGS
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 verbose = True
 debug = True
 
@@ -41,11 +13,7 @@ debug = True
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # APPLICATION
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dataset = Dataset("apps/setup_20240208_HA40_rep1.yml", verbose)
-
+dataset = Dataset("applications/setup.yml")
 process(dataset, verbose, debug)
-
 statistics(dataset, verbose, debug)
-
 montage(dataset, verbose, debug)
-
--- main_multiplate.py
+++ main_multiplate.py
@@ -6,32 +6,6 @@ from processing.pipeline import process
 from processing.montage import montage
 from processing.statistics import statistics
 
-
-"""
-HIGH THROUGHPUT SCREEN IMAGE PROCESSING AND QUANTIFICATION
-
-Application:
-    This main script coordinates all operations:
-    - stores information about the dataset (input/output paths).
-    - stores information about currently processed data.
-    - launches parallel processing of image-pairs.
-
-Variables:
-    - verbose : whether informative status messages are displayed
-      in the standard output.
-    - output_directory : choice for the name of the output directory.
-      The output directory is generated as a sub-directory inside the
-      input directory specified in the yml setup file.
-      Possible options:
-      * "date" : generates a new directory with current date/time.
-      * "debug" : generates/overwrites "output_debug" directory.
-      * anything else inside quotes: a user defined name
-"""
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# SETTINGS
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 verbose = True
 debug = True
 
@@ -53,11 +27,7 @@ for ifile in setup_files:
     if os.path.isfile(ifile):
         print("Processing setup file:", ifile)
 
-        dataset = Dataset(ifile, verbose)
-
+        dataset = Dataset(ifile)
         process(dataset, verbose, debug)
-
         statistics(dataset, verbose, debug)
-
         montage(dataset, verbose, debug)
-
